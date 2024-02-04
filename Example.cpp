027f47d7e428adeb8bb45ae0ab04a2116ea5fc63
AnujBhattarai7 <war39413@gmail.com>
2024-02-04 21:12:41 +0545

    SmallString Optimization and Code cleanup

---
+++ Example.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include "String.h"
+
+#define _PRINT_(x)              \
+    {                           \
+        std::cout << x << "\n"; \
+    }
+
+struct MemoryManager
+{
+    int _Allocation, _Free;
+};
+
+static MemoryManager _MemoryManager;
+
+void *operator new(size_t size)
+{
+    std::cout << "Allocation"
+              << "\n";
+    _MemoryManager._Allocation += size;
+    return malloc(size);
+}
+
+void operator delete(void *p, size_t size)
+{
+    std::cout << "Delete"
+              << "\n";
+    _MemoryManager._Free += size;
+
+    free(p);
+}
+
+static void PrintMemoryMetrics()
+{
+    _PRINT_("[MEMORY_METRICS]: Allocated: " << _MemoryManager._Allocation << " bytes..")
+    _PRINT_("[MEMORY_METRICS]: Freed: " << _MemoryManager._Free << " bytes..")
+}
+
+int main(int argc, char const *argv[])
+{
+    {
+        String _S = "Hello";
+
+        _PRINT_(_S)
+    }
+
+    std::cin.get();
+    PrintMemoryMetrics();
+
+    return 0;
+}
---
+++ String.h
@@ -0,0 +1,137 @@
+#pragma once
+
+#include <ostream>
+
+#include "Vector/Vector.h"
+#include "Array/Array.h"
+
+// The size which defines the number of characters in string for which to use static string
+#define _STRING_OPTIMIZATION_SIZE_ 60
+
+class String
+{
+public:
+    using Iterator = _Iterator_Base<char>;
+
+public:
+    String() {}
+    String(const char *_Str) { Append(_Str); }
+    ~String() {}
+
+    // Getters
+    // Const
+    inline const char *C_Str() const { return _String.Data(); }
+    inline const int Size() const { return _Size; }
+    inline const int Capacity() const { return (_String.Capacity() + _STRING_OPTIMIZATION_SIZE_); }
+
+    inline void Clear() { _Del(); }
+
+    inline Iterator begin()
+    {
+        if (_Size > 60)
+            return _String.begin();
+        return _SmallString.begin();
+    }
+
+    inline Iterator end()
+    {
+        if (_Size > 60)
+            return _String.end();
+        return (_SmallString.begin() + _Size);
+    }
+
+    inline void Push_Back(const char _C)
+    {
+        if (_Size < 60)
+            _SmallString.Push_Back(_C);
+        _String.Push_Back(_C);
+    }
+
+    void Append(const char *_C);
+
+    // Operators
+    String &operator=(const char *_C);
+
+    inline const char operator[](int i) const { return _String[i]; }
+    inline char operator[](int i) { return _String[i]; }
+
+    // For Printing
+    friend std::ostream &operator<<(std::ostream &os, String &_String)
+    {
+        for (char &i : _String)
+            os << i;
+
+        return os;
+    }
+
+private:
+    // The main Vector which stores the string
+    Vector<char> _String;
+
+    // The String optimization
+    Array<char, _STRING_OPTIMIZATION_SIZE_> _SmallString;
+
+    // The size of characters
+    int _Size = 0;
+
+    // Deletes the array
+    void _Del();
+
+    // Allows getting the Len of given char
+    int _String_Len(const char *_Str);
+};
+
+inline void String::Append(const char *_C)
+{
+    // Checks if size is enough
+    _Size = _String_Len(_C) + _Size;
+
+    if (_Size > 60)
+    {
+        _String.Reserve(_Size);
+        // Adds the characters
+        for (int i = 0; i < _String_Len(_C); i++)
+            _String.Push_Back(_C[i]);
+    }
+
+    for (int i = 0; i < _String_Len(_C); i++)
+        _SmallString.Push_Back(_C[i]);
+}
+
+inline String &String::operator=(const char *_C)
+{
+    _Size = _String_Len(_C);
+
+    if (_Size > 60)
+    {
+        _String.Reserve(_Size);
+
+        for (int i = 0; i < _Size; i++)
+        {
+            _String.Emplace_Back(_C[i]);
+        }
+
+        return *this;
+    }
+
+    for (int i = 0; i < _Size; i++)
+    {
+        _SmallString[i] = _C[i];
+    }
+
+    return *this;
+}
+
+inline void String::_Del()
+{
+    if (_Size > 60)
+        _String.Flush();
+}
+
+inline int String::_String_Len(const char *_Str)
+{
+    int Size = 0;
+    while (_Str[Size] != '\0')
+        Size++;
+    return Size;
+}
